31d6fc70ca36b6f78e0a7815cc80e9a95a02ced3
Mana68 <72667043+Mana68@users.noreply.github.com>
2020-10-10 21:11:57 +0200

    Update AsciiCompass.java

--- src/main/java/com/massivecraft/factions/util/AsciiCompass.java
+++ src/main/java/com/massivecraft/factions/util/AsciiCompass.java
@@ -10,13 +10,13 @@ public class AsciiCompass {
     public enum Point {
 
         N('N'),
-        NE('/'),
+        NE('\'),
         E('E'),
-        SE('\\'),
+        SE('/'),
         S('S'),
-        SW('/'),
+        SW('\'),
         W('W'),
-        NW('\\');
+        NW('/');
 
         public final char asciiChar;
 
